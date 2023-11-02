f3dda075a35b2d8375355ede8f47fcb0ece56984
JSkruls <62695130+JSkruls@users.noreply.github.com>
2023-11-02 12:36:55 +0200

    Add grid selection button
    Button cycles through three different sizes of a grid.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,39 @@
-function createCells() {
-  const cellArray = [];
+let state = 1;
 
+function createCells() {
+  const cellArray = []; 
   for(i = 0; i <= 3248; i++) {
     cellArray.push('<div class="cell-small"></div>');
   }
-
   const htmlCells = cellArray.reduce((acc,cur) => acc + cur);
   return grid.innerHTML = htmlCells;
-  
 }
 
-const grid = document.querySelector('.grid-small');
+function drawGrid() {
+  const cellArray = [];
+  state++;
+  if(state % 3 === 1) {
+    for(i = 0; i <= 3248; i++) {
+      cellArray.push('<div class="cell-small"></div>');
+    }
+  }
+  else if (state % 3 == 2) {
+    for(i = 0; i <= 483; i++) {
+      cellArray.push('<div class="cell-medium"></div>');
+    }
+  }
+  else {
+    for(i = 0; i <= 168; i++) {
+      cellArray.push('<div class="cell-large"></div>');
+    }
+  }
+  
+  const htmlCells = cellArray.reduce((acc,cur) => acc + cur);
+  return grid.innerHTML = htmlCells;
+}
+
+const grid = document.querySelector('.grid-area');
+const gridBtn = document.querySelector('.grid-btn');
+
+gridBtn.addEventListener('click',drawGrid);
 window.addEventListener('load',createCells);
